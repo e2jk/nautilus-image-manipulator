18ade92265025e88d2fe0a72141ab768687a82b1
Emilien Klein <emilien@klein.st>
2011-10-22 21:24:22 +0200

    Update version number to 0.4

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ def nautilus_plugin():
 
 DistUtilsExtra.auto.setup(
     name='nautilus-image-manipulator',
-    version='0.3',
+    version='0.4',
     license='GPL-3',
     author='Emilien Klein',
     author_email='emilien _AT_ klein _DOT_ st',
