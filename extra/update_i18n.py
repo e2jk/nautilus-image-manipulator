709bb98826561f4141b057a944fe61a03b9db4f2
Emilien Klein <emilien@klein.st>
2012-02-14 22:29:04 +0100

    Automatically extract new translations if a launchpad-export.tar.gz file is present in the ./po folder

--- extra/update_i18n.py
+++ extra/update_i18n.py
@@ -15,21 +15,47 @@
 
 import os
 import shutil
+import tarfile
+
+def po_files(members):
+    for tarinfo in members:
+        if os.path.splitext(tarinfo.name)[1] == ".po":
+            yield tarinfo
 
 def main():
     poFolder = "./po"
-    
-    try:
-        import DistUtilsExtra.auto
-    except ImportError:
-        print "ERROR: DistUtilsExtra is not installed."
-        exit(-1)
     
     # Check if the folder exists, i.e. uiFolder is a valid folder
     if not os.path.isdir(poFolder):
         # The folder does not exist, we are probably not running the script from the root of the repository
         # Exit with an error message
         print "ERROR: Please run this script from the root of the repository."
+        exit(-1)
+    
+    # Check if there's a tarball containing the most recent translation
+    lp_export_file = "%s/launchpad-export.tar.gz" % poFolder
+    if os.path.isfile(lp_export_file):
+        print "Translation tarball present: extracting new .po files"
+        # Extract all .po files
+        tar = tarfile.open(lp_export_file, "r:gz")
+        tar.extractall(members=po_files(tar))
+        tar.close()
+        
+        # Rename all the extracted .po files
+        for temp in os.walk(poFolder):
+            for f in temp[2]:
+                if "nautilus-image-manipulator-" in f:
+                    os.rename("%s/%s" % (poFolder, f),
+                              "%s/%s" % (poFolder, f.replace("nautilus-image-manipulator-", "")))
+        
+        # Delete the tarball
+        os.remove(lp_export_file)
+    
+    # Check if DistUtilsExtra is installed
+    try:
+        import DistUtilsExtra.auto
+    except ImportError:
+        print "ERROR: DistUtilsExtra is not installed."
         exit(-1)
     
     # Temporarily rename the nautilus extension so that it gets analyzed for translations
