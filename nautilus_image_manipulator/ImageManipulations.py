0e5fda230ff96cf2853c00e7f089a4e1884b14b0
Robert Bruce Park <robert.park@canonical.com>
2012-12-31 15:13:44 -0800

    Whitespace cleanup.

--- nautilus_image_manipulator/ImageManipulations.py
+++ nautilus_image_manipulator/ImageManipulations.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 ### BEGIN LICENSE
 # Copyright (C) 2010-2012 Emilien Klein <emilien _AT_ klein _DOT_ st>
-# 
-# This program is free software: you can redistribute it and/or modify it 
-# under the terms of the GNU General Public License version 3, as published 
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
 # by the Free Software Foundation.
-# 
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranties of 
-# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
 # PURPOSE.  See the GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License along 
+#
+# You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
@@ -91,7 +91,7 @@ class ImageManipulations(GObject.GObject):
 
     def resize_one_image(self, fileName):
         """Performs the resizing operation on one image.
-        
+
         The return value indicates if this resizing operation was successful.
         """
         logging.debug('resizing image: %s' % fileName)
