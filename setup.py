f13728749e1fb50a3e8f1e55d7a0fcf9fedeb3cf
Emilien Klein <emilien@klein.st>
2013-02-19 21:25:26 +0100

    Releasing version 1.2

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ def nautilus_plugin():
 
 DistUtilsExtra.auto.setup(
     name='nautilus-image-manipulator',
-    version='1.1',
+    version='1.2',
     license='GPL-3',
     author='Emilien Klein',
     author_email='emilien _AT_ klein _DOT_ st',
