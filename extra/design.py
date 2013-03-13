7d3777a054085ae94c67026992463ffa297257e8
Emilien Klein <emilien@klein.st>
2013-03-13 20:15:00 +0100

    Update the Glade file to be able to run with glade-gtk2. Note that saving from glade-gtk2 will corrupt the file! Need to port to Gtk3 I guess...

--- extra/design.py
+++ extra/design.py
@@ -21,7 +21,7 @@ def main():
     uiFolder = "./data/ui"
     
     #TODO: better not use the "/usr/bin/" part of the command
-    cmd = "/usr/bin/glade"
+    cmd = "/usr/bin/glade-gtk2"
     if not os.path.exists(cmd):
         print "ERROR: Please check your Glade installation."
         exit(-1)
